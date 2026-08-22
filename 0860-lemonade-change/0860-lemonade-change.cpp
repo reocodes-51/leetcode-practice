ae97eade134aa807018e31c3d680e8311dfa02dc
Rajyavardhan Singh Rathore <pinsubanna5@gmail.com>
2026-08-22 16:51:01 +0530

    Time: 0 ms (100%), Space: 87.1 MB (76%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int c5 = 0 ;
+        int c10 = 0 ;
+
+        for ( int i=0 ; i<bills.size() ; i++ ) {
+            if ( bills[i] == 5 ) {
+                c5++;
+            }
+            else if ( bills[i] == 10 ) {
+                if ( c5 > 0 ) {
+                    c5--;
+                    c10++;
+                }
+                else {
+                    return false ;
+                }
+            }
+            else {
+                if ( c5 > 0 && c10 > 0 ) {
+                    c5--;
+                    c10--;
+                }
+                else if ( c5 >= 3 ) {
+                    c5-=3 ;
+                }
+                else {
+                    return false ;
+                }
+            }
+           
+        }
+        return true ;
+    }
+};
