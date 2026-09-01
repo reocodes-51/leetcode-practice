cdd046bae900a1ea974c3c0de73a05bb07978d7d
Rajyavardhan Singh Rathore <pinsubanna5@gmail.com>
2026-09-01 23:16:58 +0530

    Time: 0 ms (100%), Space: 9.9 MB (51.06%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+
+        priority_queue<int> pq;
+
+        for(int x : stones) {
+            pq.push(x);
+        }
+
+        while(pq.size() > 1) {
+
+            int y = pq.top();
+            pq.pop();
+
+            int x = pq.top();
+            pq.pop();
+
+            if(x != y) {
+                pq.push(y - x);
+            }
+        }
+
+        if(pq.empty()) {
+            return 0;
+        }
+
+        return pq.top();
+    }
+};
